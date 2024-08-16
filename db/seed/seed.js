65f0b043fbffa238cc017dec8d09a2d74fdc4128
= <joshuaraftery99@gmail.com>
2024-08-16 14:00:26 +0100

    User seed added, ingredient seed in progress

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './.env.test' }); 
 const recipeTestData=require("../data/test/recipeWithIDSample.json") // hardcoded test data, make programatic later
-const usersTestData=require('../data/test/usersData') // hardcoded test data, make programatic later
+const usersTestData= require('../data/test/userTestData.json') // hardcoded test data, make programatic later
+const ingredientsTestData=require('../data/test/ingredientsData.json')
 const mongoURI = process.env.MONGO_URI;
 const Recipes = require('../../models/recipeSchema');
 const Users = require('../../models/usersSchema')
+const Ingredients = require('../../models/usersSchema')
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
@@ -33,11 +35,11 @@ async function seedDB() {
         await Users.insertMany(usersTestData);
         console.log('Users seeded successfully');
 
-        // await Ingredients.deleteMany({});
-        // console.log('Existing users removed');
+        await Ingredients.deleteMany({});
+        console.log('Existing ingredients removed');
 
-        // await Ingredients.insertMany(ingredientsTestData);
-        // console.log('Users seeded successfully');
+        await Ingredients.insertMany(ingredientsTestData);
+        console.log('ingredients seeded successfully');
 
     } catch (err) {
         console.error('Error seeding data:', err);
--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const usersSchema = new mongoose.Schema({
-  user_id: { type: Number, required: true },
-  username: { type: String, required: true },
+const userSchema = new mongoose.Schema({
+  user_id: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   avatar_url: { type: String, required: true },
-  favourite_recipes: [{ type: String, default: false }],
-  ingredients_used: [{ type: String, default: false }],
+  ingredients_used: [{ type: String }], // Array of ingredient names
+  favourite_recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }] // Array of references to Recipe model
 });
 
-module.exports = mongoose.model('Users', usersSchema);
+module.exports = mongoose.model('User', userSchema);
