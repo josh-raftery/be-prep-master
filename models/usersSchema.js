82cc9e61d8c2e681738a66fb1e927a1557c429a8
Esme <esmereldaroses@gmail.com>
2024-08-19 10:48:45 +0100

    add counter for id

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Counter = require('./idCounterSchema');
 
 const userSchema = new mongoose.Schema({
   user_id: { type: Number, required: true, unique: true },
@@ -7,6 +8,23 @@ const userSchema = new mongoose.Schema({
   avatar_url: { type: String, required: true },
   ingredients_used: [{ type: String }], // Array of ingredient names
   favourite_recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }] // Array of references to Recipe model
+});
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (user.isNew) {
+    try {
+      const counter = await Counter.findByIdAndUpdate(
+        { _id: 'user_id' },
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      );
+      user.user_id = counter.seq;
+    } catch (error) {
+      return next(error);
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
