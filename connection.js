c5f2bc48ca6f760e739eb6f057830792b4c01c29
Bryan Rafosala <bryanrafosala@Bryans-Laptop.northcoders.com>
2024-08-20 16:53:11 +0100

    parmetric endpoints & testing

--- connection.js
+++ connection.js
@@ -1,4 +1,6 @@
 const ENV = process.env.NODE_ENV || 'development';
+
+
 require('dotenv').config({
     path: `${__dirname}/.env.${ENV}`,
 });
---
+++ src/app/api/recipes/[recipe_id]/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { getRecipeById } from "../../../../../controller/recipeController";
+
+export async function GET (request) {
+    const splitUrl = request.url.split('/')
+    const recipe_id = splitUrl[splitUrl.length - 1]
+    return getRecipeById(recipe_id)
+    .then ((recipe)=>{
+        return  NextResponse.json(recipe, {status:200})
+    })
+
+}
