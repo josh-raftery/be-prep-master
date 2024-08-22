a2502d7146bfb0ef9e1354b42abfeca9d9739e48
OpaogunProsper <prblvck@gmail.com>
2024-08-22 16:34:01 +0100

    Patch recipes

--- src/app/api/recipes/[recipe_id]/route.js
+++ src/app/api/recipes/[recipe_id]/route.js
@@ -6,8 +6,19 @@ export async function GET (request) {
     return getRecipeById(recipe_id)
 }
 
-export async function PATCH (request){
-    const splitUrl = request.url.split('/')
-    const recipe_id = splitUrl[splitUrl.length - 1]
-    return patchRecipe(recipe_id)
+export async function PATCH(request) {
+  try {
+    const url = new URL(request.url);
+    const splitUrl = url.pathname.split("/");
+    const recipe_id = splitUrl[splitUrl.length - 1];
+
+    const updateData = await request.json();
+
+    return await patchRecipe(recipe_id, updateData);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
