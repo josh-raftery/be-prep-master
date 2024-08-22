be399252c0c7c8ff83f4336f510210e2e3183744
Esme <esmereldaroses@gmail.com>
2024-08-22 16:49:53 +0100

    updated ui on signup page

--- src/app/(avatar menu)/profile/[user_id]/page.jsx
+++ src/app/(avatar menu)/profile/[user_id]/page.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
-
 export default async function Profile({ params }) {
   const host = process.env.HOST || "localhost";
   const port = process.env.PORT || 3000;
@@ -17,22 +16,25 @@ export default async function Profile({ params }) {
 
   return (
     <>
-      <container className="flex flex-col items-center pb-10 m-4">
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-10">
-          <div className="flex flex-col items-center pb-10 m-4">
+      <div className="flex flex-col items-center pb-10 m-4">
+        <container className="card bg-white max-w-3xl shadow-xl flex-col items-center">
+          <h2 className="mb-1 text-xl font-medium text-gray-900 dark:text-white m-2 flex flex-col items-center">
+              Hi {user.name}!
+          </h2>
+          <figure className="px-10 pt-10">
             <img
               alt="avatar image"
-              className="w-24 h-24 mb-3 rounded-full shadow-lg"
+              className="w-48 h-48 mb-3 rounded-full shadow-lg"
               src={user.avatar_url}
             />
-            <h2 className="mb-1 text-xl font-medium text-gray-900 dark:text-white m-2">
-              Hi {user.name}!
-            </h2>
-            <h3 className="text-md text-gray-500 dark:text-gray-400 m-2">
-              Username: {user.username}
-            </h3>
-
-            <container id="scores-section">
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Scores!</h2>
+            <p>Username: {user.username}</p>
+          </div>
+          
+          
+          <section id="scores-section">
               <h3 className="text-md text-gray-900  m-2">Scores</h3>
               <section className="rating rating-md m-2" id="stars-section">
                 <input
@@ -64,7 +66,7 @@ export default async function Profile({ params }) {
                   className="mask mask-star-2 bg-orange-400"
                 />
               </section>
-              <section id ="hearts-section" className ="m-2">
+              <section id="hearts-section" className="m-2">
                 <div className="rating gap-1">
                   <input
                     type="radio"
@@ -94,34 +96,42 @@ export default async function Profile({ params }) {
                   />
                 </div>
               </section>
-            </container>
-            <div className="card-actions justify-end">
-              {/* insert link or navigation to edit profile page */}
-              <button className="btn bg-primary mt-4">
-                <Image
-                src="/editUserIcon.png"
-                alt="edit profile icon"
-                width={20}
-                height={20}
-                />
-                     Edit my profile
-              </button>
-            </div>
-            <div className="card-actions justify-end">
-              {/* insert link or navigation to favouriteRecipes */}
-              <button className="btn bg-secondary mt-4">
-                <Image
-                src="/heartIcon.png"
-                alt="heart icon"
-                width={20}
-                height={20}
-                />
-                     My Favourite Recipes
-              </button>
-            </div>
-          </div>
-        </div>
-      </container>
+            </section>
+            <section> 
+              <div className="card-actions justify-center">
+                {/* insert link or navigation to edit profile page */}
+                <button className="btn bg-primary mt-4">
+                  <Image
+                    src="/editUserIcon.png"
+                    alt="edit profile icon"
+                    width={20}
+                    height={20}
+                  />
+                  Edit my profile
+                </button>
+              </div>
+              <div className="card-actions justify-center">
+                {/* insert link or navigation to favouriteRecipes */}
+                <button className="btn bg-secondary mt-4">
+                  <Image
+                    src="/heartIcon.png"
+                    alt="heart icon"
+                    width={20}
+                    height={20}
+                  />
+                  My Favourite Recipes
+                </button>
+              </div>
+            </section>
+        </container>  
+      </div>
+
+
+
+
+
+
+
     </>
   );
 }
