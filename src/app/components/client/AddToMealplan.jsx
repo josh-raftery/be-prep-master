e32be1992a7be2777bbec261a1d98bee8b5af8d0
= <joshuaraftery99@gmail.com>
2024-08-26 17:28:00 +0100

    Add to meal plan component added, some progress made

---
+++ src/app/components/client/AddToMealplan.jsx
@@ -0,0 +1,34 @@
+'use client'
+import { useState} from "react"
+import MealPlan from "src/app/mealplan/page";
+
+export default function AddToMealPlan({recipe}){
+
+    const [diff, setDiff] = useState(0)
+    const [servingsToAllocate,setServingsToAllocate] = useState(recipe.serves)
+
+    function handleClick(){
+        setDiff((currDiff) => {
+            if(currDiff === 0){
+                return 1
+            } else{
+                return 0
+            }
+        })
+    }
+
+    return (
+        <div className="mealplan-popup" >
+            <div className="card bg-primary text-primary-content w-96">
+                <div className="card-body">
+                    <h2 className="card-title">Meal Plan<button onClick={handleClick} className="btn btn-outline" style={{marginLeft: "7rem"}} >{diff === 0 ? 'next week' : 'this week'}</button></h2>
+                    <h2 style={{marginTop: "1rem"}} className="card-title">{`New Meal: ${recipe.title}`}</h2>
+                    <h2 className="card-title"><b>{`Select ${servingsToAllocate} slots`}</b></h2>
+                    <div className="card-actions justify-end">
+                        <MealPlan diff={diff}  popUp={true} servingsToAllocate={servingsToAllocate} setServingsToAllocate={setServingsToAllocate} recipeToAdd={recipe}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+)
+}
---
+++ src/app/components/client/MealGeneratorDays.jsx
@@ -0,0 +1,162 @@
+"use client";
+
+import { getRecipeById } from "api";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Link from "next/link";
+import calculateNutrition from "src/utils/calculateNutrition";
+
+export default function MealGeneratorDays({ day, date, mealPlan, setServingsToAllocate, servingsToAllocate, recipeToAdd }) {
+  const [todaysMeals, setTodaysMeals] = useState([]);
+  const [todaysRecipes, setTodaysRecipes] = useState([]);
+  const [hasFetched, setHasFetched] = useState(false);
+  const [todaysBreakfast, setTodaysBreakfast] = useState([]);
+  const [todaysLunch, setTodaysLunch] = useState([])
+  const [todaysDinner, setTodaysDinner] = useState([]);
+  const [todaysSnacks, setTodaysSnacks] = useState([]);
+  const [todaysDessert, setTodaysDessert] = useState([]);
+  const [newMeals,setNewMeals] = useState([])
+
+  function addToPopUp(mealType,recipe){
+    if (mealType === "breakfast") {
+      setTodaysBreakfast((currBreakfast) => {
+        return [...currBreakfast, recipe];
+      });
+    }
+    if (mealType === "lunch") {
+      setTodaysLunch((currLunch) => {
+        return [...currLunch, recipe];
+      });
+    }
+    if (mealType === "dinner") {
+      setTodaysDinner((currDinner) => {
+        return [...currDinner, recipe];
+      });
+    }
+    if (mealType === "snacks") {
+      setTodaysSnacks((currSnacks) => {
+        return [...currSnacks, recipe];
+      });
+    }
+    if (mealType === "dessert") {
+      setTodaysDessert((currDessert) => {
+        return [...currDessert, recipe];
+      });
+    }
+  }
+
+  useEffect(() => {
+    const todaysMealsInput = mealPlan.meals.filter((meal) => {
+      if (meal.date === date) {
+        return meal;
+      }
+    });
+    setTodaysMeals(todaysMealsInput);
+  }, []);
+
+  useEffect(() => {
+    const promisesArray = todaysMeals.map((meal) => {
+      return getRecipeById(meal.recipe_id);
+    });
+    Promise.all(promisesArray).then((recipes) => {
+      recipes.forEach((recipe, index) => {
+        addToPopUp(todaysMeals[index].mealType,recipe)
+      });
+      setTodaysRecipes(recipes);
+      setHasFetched(true);
+      const currNutrition = calculateNutrition(recipes);
+      setNutrition(currNutrition);
+    });
+  }, [todaysMeals]);
+
+  if (!hasFetched) {
+    return <Loading />;
+  }
+
+  function handleClick(mealInfo){
+    
+    
+  }
+
+  if (hasFetched) {
+    console.log(todaysRecipes, " todaysRecipes");
+    console.log(todaysMeals, " todays meals");
+    console.log(todaysBreakfast, " breakfast");
+    console.log(todaysLunch, " breakfast");
+      return (
+
+      <div className="card bg-primary text-primary-content w-96">
+        <div className="card-body">
+        <h2 className="card-title">{`${day}, ${date}`}</h2>
+          <div className="checkbox-container" >
+            <h3 style={{ marginTop: "1rem" }} className="card-title">
+              Breakfast:
+            </h3>
+            <input onClick={() => {handleClick({mealType: "breakfast", date })}} style={{marginLeft: "250px", marginTop: "1.2rem"}} type="checkbox" className="checkbox-custom checkbox" />
+          </div>
+          {todaysBreakfast.length > 0 ? (
+            todaysBreakfast.map((breakfast) => {
+              return <p>{breakfast.title}</p>
+            })
+          ) : (
+            <p>No breakfast</p>
+          )}
+          <div className="checkbox-container" >
+              <h3 style={{ marginTop: "1rem" }} className="card-title">
+                Lunch:
+              </h3>
+              <input onClick={() => {handleClick({mealType: "lunch", date })}} style={{marginLeft: "285px", marginTop: "1.2rem"}} type="checkbox" className="checkbox-custom checkbox" />
+          </div>
+            
+          {todaysLunch.length > 0 ? (
+            todaysLunch.map((lunch) => {
+              return <p>{lunch.title}</p>
+            })
+          ) : (
+            <p>No Lunch</p>
+          )}
+          <div className="checkbox-container" >
+            <h3 style={{ marginTop: "1rem" }} className="card-title">
+              Dinner:
+            </h3>
+            <input onClick={() => {handleClick({mealType: "dinner", date })}} style={{marginLeft: "277px", marginTop: "1.2rem"}} type="checkbox" className="checkbox-custom checkbox" />
+          </div>
+          {todaysDinner.length > 0 ? (
+            todaysDinner.map((dinner) => {
+              return <p>{dinner.title}</p>
+            })
+          ) : (
+            <p>No Dinner</p>
+          )}
+          <div className="checkbox-container" >
+            <h3 style={{ marginTop: "1rem" }} className="card-title">
+              Snacks:
+            </h3>
+            <input onClick={() => {handleClick({mealType: "snacks", date })}} style={{marginLeft: "277px", marginTop: "1.2rem"}} type="checkbox" className="checkbox-custom checkbox" />
+          </div>
+          {todaysSnacks.length > 0 ? (
+            todaysSnacks.map((snack) => {
+              return <p>{snack.title}</p>
+            })
+          ) : (
+            <p>No Snacks</p>
+          )}
+          <div className="checkbox-container" >
+            <h3 style={{ marginTop: "1rem" }} className="card-title">
+              Dessert:
+            </h3>
+            <input onClick={() => {handleClick({mealType: "dessert", date })}} style={{marginLeft: "270px", marginTop: "1.2rem"}} type="checkbox" className="checkbox-custom checkbox" />
+          </div>
+          {todaysDessert.length > 0 ? (
+            todaysDessert.map((dessert) => {
+              return <p>{dessert.title}</p>
+            })
+          ) : (
+            <p>No Dinner</p>
+          )}
+        </div>
+      </div>
+      )
+  }
+}
+
