45a730ae0e1381056959f14f33b6c71f561e4200
OpaogunProsper <prblvck@gmail.com>
2024-08-27 16:55:59 +0100

    couple queries done and mealplan gen styling

--- src/app/components/client/Loading.jsx
+++ src/app/components/client/Loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading(){
     return (
-        <div className="loading-animation" >
+        <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-white bg-opacity-80 z-50" >
             <span className="loading loading-ball loading-lg"></span>
         </div>
     )
