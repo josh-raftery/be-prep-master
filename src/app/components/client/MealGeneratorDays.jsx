3cfcaa95448260d1742debbed7dd9271ed4636c6
= <joshuaraftery99@gmail.com>
2024-08-28 15:04:02 +0100

    Meal Generator Adding to MealPlan DB

--- src/app/components/client/MealGeneratorDays.jsx
+++ src/app/components/client/MealGeneratorDays.jsx
@@ -1,59 +1,78 @@
 "use client";
 
-import { getRandomRecipe, getRecipeById, getRecipes } from "api";
+import {  getRecipeById } from "api";
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
-import Link from "next/link";
-import calculateNutrition from "src/utils/calculateNutrition";
 
-export default function MealGeneratorDays({ day, date, mealPlan, setServingsToAllocate, servingsToAllocate, recipeToAdd }) {
+export default function MealGeneratorDays({
+  day,
+  date,
+  mealPlan,
+  setServingsToAllocate,
+  servingsToAllocate,
+  recipeToAdd,
+  setbreakfastServings,
+  setNewBreakfasts,
+  setlunchServings,
+  setNewLunch,
+  setdinnerServings,
+  setNewDinners,
+  setdessertServings,
+  setNewDesserts,
+  setsnacksServings,
+  setNewSnacks
+}) {
   const [todaysMeals, setTodaysMeals] = useState([]);
   const [todaysRecipes, setTodaysRecipes] = useState([]);
   const [hasFetched, setHasFetched] = useState(false);
   const [todaysBreakfast, setTodaysBreakfast] = useState([]);
-  const [todaysLunch, setTodaysLunch] = useState([])
+  const [todaysLunch, setTodaysLunch] = useState([]);
   const [todaysDinner, setTodaysDinner] = useState([]);
   const [todaysSnacks, setTodaysSnacks] = useState([]);
   const [todaysDessert, setTodaysDessert] = useState([]);
-  const [newBreakfasts, setNewBreakfasts] = useState([])
-  const [newLunch, setNewLunch] = useState([])
-  const [newDinners, setNewDinners] = useState([])
-  const [newSnacks, setNewSnacks] = useState([])
-  const [newDesserts, setNewDesserts] = useState([])
-  const [mealQueries,setMealQueries] = useState([])
-  const [breakfastServings,setbreakfastServings] = useState(0)
-  const [lunchServings,setlunchServings] = useState(0)
-  const [dinnerServings,setdinnerServings] = useState(0)
-  const [snacksServings,setsnacksServings] = useState(0)
-  const [dessertServings,setdessertServings] = useState(0)
 
-  function addToPopUp(mealType,recipe){
+  function addToPopUp(mealType, recipe) {
+    const addMealIfNotExists = (currentMeals, setMeals) => {
+      const alreadyExists = currentMeals.some(
+        (existingRecipe) => existingRecipe.recipe_id === recipe.recipe_id
+      );
+      if (!alreadyExists) {
+        setMeals([...currentMeals, recipe]);
+      }
+    };
+  
     if (mealType === "breakfast") {
       setTodaysBreakfast((currBreakfast) => {
-        return [...currBreakfast, recipe];
+        addMealIfNotExists(currBreakfast, setTodaysBreakfast);
+        return currBreakfast;
       });
     }
     if (mealType === "lunch") {
       setTodaysLunch((currLunch) => {
-        return [...currLunch, recipe];
+        addMealIfNotExists(currLunch, setTodaysLunch);
+        return currLunch;
       });
     }
     if (mealType === "dinner") {
       setTodaysDinner((currDinner) => {
-        return [...currDinner, recipe];
+        addMealIfNotExists(currDinner, setTodaysDinner);
+        return currDinner;
       });
     }
     if (mealType === "snacks") {
       setTodaysSnacks((currSnacks) => {
-        return [...currSnacks, recipe];
+        addMealIfNotExists(currSnacks, setTodaysSnacks);
+        return currSnacks;
       });
     }
     if (mealType === "dessert") {
       setTodaysDessert((currDessert) => {
-        return [...currDessert, recipe];
+        addMealIfNotExists(currDessert, setTodaysDessert);
+        return currDessert;
       });
     }
   }
+  
 
   useEffect(() => {
     const todaysMealsInput = mealPlan.meals.filter((meal) => {
@@ -61,8 +80,13 @@ export default function MealGeneratorDays({ day, date, mealPlan, setServingsToAl
         return meal;
       }
     });
+    setTodaysBreakfast([])
+    setTodaysLunch([])
+    setTodaysDinner([])
+    setTodaysSnacks([])
+    setTodaysDessert([])
     setTodaysMeals(todaysMealsInput);
-  }, []);
+  }, [mealPlan]);
 
   useEffect(() => {
     const promisesArray = todaysMeals.map((meal) => {
@@ -70,120 +94,57 @@ export default function MealGeneratorDays({ day, date, mealPlan, setServingsToAl
     });
     Promise.all(promisesArray).then((recipes) => {
       recipes.forEach((recipe, index) => {
-        addToPopUp(todaysMeals[index].mealType,recipe)
+        addToPopUp(todaysMeals[index].mealType, recipe);
       });
       setTodaysRecipes(recipes);
       setHasFetched(true);
     });
   }, [todaysMeals]);
 
-  if (!hasFetched) {
-    return <Loading />;
-  }
-
-  function onSubmit(){
-    console.log(breakfastServings, 'breakfast serve')
-    console.log(lunchServings, 'lunch serve')
-    console.log(dinnerServings, 'dinner serve')
-    console.log(snacksServings, 'snack serve')
-    console.log(dessertServings, 'dessert serve')
-
-    const queries = []
-
-    // if(breakfastServings){
-    //   queries.push(getRandomRecipe({mealType: 'breakfast', serves: breakfastServings}))
-    // }
-
-    // if(lunchServings){
-    //   queries.push(getRandomRecipe({mealType: 'lunch', serves: lunchServings}))
-    // }
-
-    // if(dinnerServings){
-    //   queries.push(getRandomRecipe({mealType: 'dinner', serves: dinnerServings}))
-    // }
-
-    // if(snacksServings){
-    //   queries.push(getRandomRecipe({mealType: 'snack', serves: snacksServings}))
-    // }
-
-    // if(dessertServings){
-    //   queries.push(getRecipes({mealType: 'dessert', serves: dessertServings}))
-    // }
-
-    // Promise.all(queries)
-    // .then((recipesArray) => {
-    //   recipesArray.forEach((mealTypeArray,typeIndex) => {
-    //     mealTypeArray.forEach((recipe,mealIndex) => {
-    //       if(typeIndex === 0){ // breakfast
-    //         setNewBreakfasts((currBreakfast) => {
-    //           currBreakfast[mealIndex].recipe_id = recipe.recipe_id
-    //         })
-    //       }
-    //       if(typeIndex === 1){ // lunch
-    //         setNewLunch((currLunch) => {
-    //           currLunch[mealIndex].recipe_id = recipe.recipe_id
-    //         })
-    //       }
-    //       if(typeIndex === 2){ // dinner
-    //         setNewDinners((currDinner) => {
-    //           currDinner[mealIndex].recipe_id = recipe.recipe_id
-    //         })
-    //       }
-    //       if(typeIndex === 3){ // snacks
-    //         setNewSnacks((currSnacks) => {
-    //           currSnacks[mealIndex].recipe_id = recipe.recipe_id
-    //         })
-    //       }
-    //       if(typeIndex === 4){ // dessert
-    //         setNewDesserts((currDessert) => {
-    //           currDessert[mealIndex].recipe_id = recipe.recipe_id
-    //         })
-    //       }
-    //     })
-    //   })
-    // })
-  }
-
-  function handleClick(mealInfo){
-    if(mealInfo.mealType === 'breakfast'){
+  function handleClick(mealInfo) {
+    if (mealInfo.mealType === "breakfast") {
       setbreakfastServings((currServings) => {
-        return currServings + 1
-      })
+        return currServings + 1;
+      });
       setNewBreakfasts((currBreakfast) => {
-        return [...currBreakfast,mealInfo]
-      })
+        return [...currBreakfast, mealInfo];
+      });
     }
-    if(mealInfo.mealType === 'lunch'){
+    if (mealInfo.mealType === "lunch") {
       setlunchServings((currServings) => {
-        return currServings + 1
-      })
+        return currServings + 1;
+      });
       setNewLunch((currLunch) => {
-        return [...currLunch,mealInfo]
-      })
+        return [...currLunch, mealInfo];
+      });
     }
-    if(mealInfo.mealType === 'dinner'){
+    if (mealInfo.mealType === "dinner") {
       setdinnerServings((currServings) => {
-        return currServings + 1
-      })
+        return currServings + 1;
+      });
       setNewDinners((currDinner) => {
-        return [...currDinner,mealInfo]
-      })
+        return [...currDinner, mealInfo];
+      });
     }
-    if(mealInfo.mealType === 'dessert'){
+    if (mealInfo.mealType === "dessert") {
       setdessertServings((currServings) => {
-        return currServings + 1
-      })
-      setNewSnacks((currSnacks) => {
-        return [...currSnacks,mealInfo]
-      })
+        return currServings + 1;
+      });
+      setNewDesserts((currSnacks) => {
+        return [...currSnacks, mealInfo];
+      });
     }
-    if(mealInfo.mealType === 'snacks'){
+    if (mealInfo.mealType === "snacks") {
       setsnacksServings((currServings) => {
-        return currServings + 1
-      })
-      setNewDesserts((currDessert) => {
-        return [...currDessert,mealInfo]
-      })
+        return currServings + 1;
+      });
+      setNewSnacks((currDessert) => {
+        return [...currDessert, mealInfo];
+      });
+    }
+
+    if (!hasFetched) {
+      return <Loading />;
     }
 
     // build array of mealtypes to query
@@ -194,9 +155,7 @@ export default function MealGeneratorDays({ day, date, mealPlan, setServingsToAl
     //add recipe ID to newMeal state
     //send request to mealplan
     //re-render page
-    
   }
-
   if (hasFetched) {
       return (
         <div className="card bg-primary text-primary-content w-96">
@@ -303,22 +262,6 @@ export default function MealGeneratorDays({ day, date, mealPlan, setServingsToAl
             ) : (
               <p>No Dinner</p>
             )}
-            <br />
-            <button onClick={onSubmit} className="btn ml-6 btn-outline">
-              Generate
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="size-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9.638 1.093a.75.75 0 0 1 .724 0l2 1.104a.75.75 0 1 1-.724 1.313L10 2.607l-1.638.903a.75.75 0 1 1-.724-1.313l2-1.104ZM5.403 4.287a.75.75 0 0 1-.295 1.019l-.805.444.805.444a.75.75 0 0 1-.724 1.314L3.5 7.02v.73a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 1 .388-.657l1.996-1.1a.75.75 0 0 1 1.019.294Zm9.194 0a.75.75 0 0 1 1.02-.295l1.995 1.101A.75.75 0 0 1 18 5.75v2a.75.75 0 0 1-1.5 0v-.73l-.884.488a.75.75 0 1 1-.724-1.314l.806-.444-.806-.444a.75.75 0 0 1-.295-1.02ZM7.343 8.284a.75.75 0 0 1 1.02-.294L10 8.893l1.638-.903a.75.75 0 1 1 .724 1.313l-1.612.89v1.557a.75.75 0 0 1-1.5 0v-1.557l-1.612-.89a.75.75 0 0 1-.295-1.019ZM2.75 11.5a.75.75 0 0 1 .75.75v1.557l1.608.887a.75.75 0 0 1-.724 1.314l-1.996-1.101A.75.75 0 0 1 2 14.25v-2a.75.75 0 0 1 .75-.75Zm14.5 0a.75.75 0 0 1 .75.75v2a.75.75 0 0 1-.388.657l-1.996 1.1a.75.75 0 1 1-.724-1.313l1.608-.887V12.25a.75.75 0 0 1 .75-.75Zm-7.25 4a.75.75 0 0 1 .75.75v.73l.888-.49a.75.75 0 0 1 .724 1.313l-2 1.104a.75.75 0 0 1-.724 0l-2-1.104a.75.75 0 1 1 .724-1.313l.888.49v-.73a.75.75 0 0 1 .75-.75Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       );
