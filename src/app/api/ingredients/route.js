2f4b4db4d12c9c35b17c4136ab1c8c818515ac4f
= <joshuaraftery99@gmail.com>
2024-09-11 11:56:18 +0100

    deleted ingredients, updated packages

--- src/app/api/ingredients/route.js
+++
@@ -1,9 +0,0 @@
-import { getIngredients } from "controller/ingredientController";
-
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return getIngredients().then((ingredients) => {
-    return NextResponse.json(ingredients, { status: 200 });
-  });
-}
