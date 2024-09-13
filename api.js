99d2038ad78f89ebd8270b92db3f65c9b3f324e7
= <joshuaraftery99@gmail.com>
2024-09-13 11:06:44 +0100

    Changed URL to hosted url

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
 
-const baseUrl = `http://${host}:${port}/api`;
+const baseUrl = `https://be-prep-master.vercel.app/api`;
 
 
 const api = axios.create({
