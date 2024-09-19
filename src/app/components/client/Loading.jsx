6f732af99e1389d10b03448a88551a15f59dbbef
Bryan Rafosala <bryanrafosala@Bryans-Laptop.local>
2024-09-19 10:22:51 +0100

    updated lottie animation

--- src/app/components/client/Loading.jsx
+++ src/app/components/client/Loading.jsx
@@ -1,20 +1,21 @@
-// 'use client'
-// import React from 'react';
-// import animationData from '../../../../public/loading-animation.json';
+'use client'
+import React from 'react';
+import animationData from '../../../../public/loading-animation.json';
+import dynamic from 'next/dynamic';
 
-// import Lottie from "lottie-react"
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
 export default function Loading() {
   return (
-    <h1>loading...</h1>
-    // <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-white bg-opacity-80 z-50">
-    //   <Lottie
-    //     autoplay
-    //     loop
-    //     animationData={animationData} // Use animationData instead of src
-    //     style={{ height: '400px', width: '400px' }} // Adjust size as needed
-    //   />
-    // </div>
+    // <h1>loading...</h1>
+    <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-white bg-opacity-80 z-50">
+      <Lottie
+        autoplay
+        loop
+        animationData={animationData} // Use animationData instead of src
+        style={{ height: '400px', width: '400px' }} // Adjust size as needed
+      />
+    </div>
   );
 }
 
