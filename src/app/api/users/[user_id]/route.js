910af9cb4cd55d2956e50ec1bee89545ab3e4dc1
OpaogunProsper <prblvck@gmail.com>
2024-09-26 16:47:09 +0100

    mvc patterned users

--- src/app/api/users/[user_id]/route.js
+++ src/app/api/users/[user_id]/route.js
@@ -1,23 +1,39 @@
-import { getUsersById, patchUser, addToMyRecipes } from 'controller/usersController';
+import { addToMyRecipesInController } from "controller/usersController";
+import { fetchUserById, updateUser } from "models/usersModel";
+import { NextResponse } from "next/server";
 
-export async function GET (request) {
-    const splitUrl = request.url.split('/')
-    const user_id = splitUrl[splitUrl.length - 1]
-    return getUsersById(user_id)
+export async function GET(request) {
+  const splitUrl = request.url.split("/");
+  const user_id = splitUrl[splitUrl.length - 1];
+  const user = await fetchUserById(user_id);
+
+  if (!user) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+  return NextResponse.json({ user }, { status: 200 });
 }
 
 export async function PATCH(request) {
-    try {
-      const url = new URL(request.url);
-      const splitUrl = url.pathname.split('/')
-      const user_id = splitUrl[splitUrl.length - 1]
-      const updateData = await request.json()
-      const myrecipes = request.nextUrl.searchParams.get("myrecipes")
-    
-      if(myrecipes === 'true'){
-        return addToMyRecipes(user_id, updateData)}
-      }catch (error) {
-          console.log(error)
-        }}
-     
-  
+  try {
+    const url = new URL(request.url);
+    const splitUrl = url.pathname.split("/");
+    const user_id = splitUrl[splitUrl.length - 1];
+    const updateData = await request.json();
+    const myrecipes = request.nextUrl.searchParams.get("myrecipes");
+
+    let response;
+
+    if (myrecipes === "true") {
+      response = await addToMyRecipesInController(user_id, updateData);
+    } else {
+      response = await updateUser(user_id, updateData);
+    }
+    return NextResponse.json(
+      { message: "Success", data: response },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "An error occurred" }, { status: 400 });
+  }
+}
